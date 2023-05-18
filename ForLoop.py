3782e0dac15e7ce0619fd0f113622f867761474c
siles-dev <siles88@gmail.com>
2023-05-18 13:44:26 -0700

    For Loops and Tuples

---
+++ ForLoop.py
@@ -0,0 +1,8 @@
+# For loop
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+# for {item in listXYZ}:
+#   {statement}
+# Declare loop variable(`item`) to represent ea item in list
+for number in nums:
+    print("The current number(`item`) is: " + str(number))
